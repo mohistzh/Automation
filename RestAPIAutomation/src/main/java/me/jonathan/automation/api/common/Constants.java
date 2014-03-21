4cafacc29bf50a0885af843b07360a51e5d4f3bc
Jonathan Zhang <mohistzh@gmail.com>
2014-03-21 17:51:41 +0800

    Add automation test project for jenkins

---
+++ RestAPIAutomation/src/main/java/me/jonathan/automation/api/common/Constants.java
@@ -0,0 +1,13 @@
+package me.jonathan.automation.api.common;
+/**
+ * A constant class for host all variables related to RestAPIWeb project
+ *
+ * @author 	Jonathan Zhang<br>
+ *			mohistzh@gmail.com
+ * @since   Mar 21, 2014
+ * @version 0.0.1-SNAPSHOT
+ */
+public class Constants {
+
+	public static final String APIAddress="http://localhost:8888";
+}
---
+++ RestAPIAutomation/src/test/java/me/jonathan/automation/api/HelloResourceTest.java
@@ -0,0 +1,17 @@
+package me.jonathan.automation.api;
+
+import me.jonathan.automation.api.common.Constants;
+
+import org.junit.Test;
+
+import com.jayway.restassured.RestAssured;
+
+public class HelloResourceTest {
+
+	private final String apiAddress = Constants.APIAddress+"/rest/myresource";
+	@Test
+	public void testMyResourceGet() {
+		String response = RestAssured.get(apiAddress).asString();
+		System.out.println(response);
+	}
+}
